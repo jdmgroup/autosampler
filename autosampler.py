4c205e3155c27f232ba6bb95e31e33e2226f7e6e
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-03-13 16:31:51 +0000

    added argparse, fixed mistake with comport

--- autosampler.py
+++ autosampler.py
@@ -1,8 +1,9 @@
 import serial
+import argparse
 
 class Autosampler:
 	def __init__(self,comport):
-		self.serialcon = serial.Serial('COM3',timeout=2)
+		self.serialcon = serial.Serial(comport,timeout=2)
 		self.check()
 
 	def advance(self):
@@ -32,3 +33,25 @@ class Autosampler:
 		
 		if not resp.startswith(expect):
 			print('ERROR: nexpected response from autosampler with check')
+
+# Command line options
+# Run with -h flag to see help
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Command line interface to control TWP\'s Arduino autosampler')
+    parser.add_argument('port',help='serial port')
+    parser.add_argument('-a',dest='advance',help='advance position',action='store_true')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-on',action='store_true',help='valve on')
+    group.add_argument('-off',action="store_true",help='valve off')
+    args = parser.parse_args()
+
+    auto = Autosampler(args.port)
+
+    if args.advance:
+        auto.advance()
+
+    if args.on:
+        auto.valve(True)
+    elif args.off:
+        auto.valve(False)
